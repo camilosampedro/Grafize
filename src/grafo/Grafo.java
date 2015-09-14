988daec375de6e3663c7353283c9e66d2dbeff31
camilo <camilo@camilo-All-Series>
2015-09-13 22:53:38 -0500

    Pequeños detalles

---
+++ src/grafo/Grafo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grafo;
+
+import com.mxgraph.view.mxGraph;
+
+/**
+ *
+ * @author camilo
+ */
+public class Grafo extends mxGraph {
+    
+}
