f0eeda2669522d0350b50755bba74ed310e95223
vanesa.guzman <vanesa.guzman@lis-sala4>
2015-09-17 14:55:15 -0500

    Modifiqué el comentario de la raíz del árbol.

--- src/modelo/Arbol.java
+++ src/modelo/Arbol.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
 
 import exception.NoEncontrado;
@@ -17,6 +12,9 @@ import java.util.logging.Logger;
  */
 public class Arbol<T> {
 
+    /**
+     * Raiz del arbol.
+     */
     private Nodo<T> raiz;
 
     /**
